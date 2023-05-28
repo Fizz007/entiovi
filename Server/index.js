3bd07fabb88f658f906074ecc7586870e7289c7c
Faisal Abrar <erfaisalabrar@gmail.com>
2023-05-28 19:52:28 +0530

    entiovi task

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const studentsData  = require('./data.json')
+const app = express();
+
+
+const PORT = 7100;
+app.listen(PORT, (req,res)=> {
+    console.log(`server is running on ${PORT}`)
+})
+
+
+app.get('/data', (req,res)=> {
+    res.send(studentsData)
+})
